22a2826d1169f29512fcb09f310c23710a5c7843
Kevin Muniz <muniz.kevin@outlook.com>
2026-04-12 19:13:56 -0400

    changes to sizing for icons on mobile/desktop

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
       }}
     >
       <div className="flex items-center justify-center gap-6 py-3, relative margin-bottom: 100px">
-        <div className="absolute left-6">
+        <div className="absolute left-3 md:left-6">
           <a
             href="mailto:muniz.kevin@outlook.com"
             style={{ fontFamily: "'Press Start 2P', monospace", fontSize: "16px", padding: "12px 24px", background: "rgba(10,0,16,0.97)", color: "#fff", border: "2px solid #aa44ff", textDecoration: "none", letterSpacing: "0.08em", display: "inline-flex", alignItems: "center", gap: "8px" }}
@@ -23,15 +23,15 @@ export default function NavBar() {
           </a>
         </div>
         <a href="https://github.com/KevinMuniz1" target="_blank" className="flex flex-col items-center gap-1">
-          <img src="/github.png" className="w-20 h-20 hover:scale-110" />
+          <img src="/github.png" className="w-10 h-10 md:w-20 md:h-20 hover:scale-110" />
           <span style={{color: "#aa44ff"}}className="text-xs">GitHub</span>
         </a>
         <a href="/KevinMunizResume.pdf" target="_blank" className="flex flex-col items-center gap-1">
-          <img src="/resumeIcon.png" className="w-20 h-20 hover:scale-110" />
+          <img src="/resumeIcon.png" className="w-10 h-10 md:w-20 md:h-20 hover:scale-110" />
           <span style={{color: "#aa44ff"}}className="text-xs">Resume</span>
         </a>
         <a href="https://www.linkedin.com/in/muniz-kevin/" target="_blank" className="flex flex-col items-center gap-1">
-          <img src="/linkedin.png" className="w-20 h-20 hover:scale-110" />
+          <img src="/linkedin.png" className="w-10 h-10 md:w-20 md:h-20 hover:scale-110" />
           <span style={{color: "#aa44ff"}}className="text-xs">LinkedIn</span>
         </a>
       </div>
