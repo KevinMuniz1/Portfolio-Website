e15afb956a0c820dcad298e3e4bf8ebbf0beceb1
Kevin Muniz <149540366+KevinMuniz1@users.noreply.github.com>
2026-04-29 20:31:45 -0400

    testing global css to see if this works

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Exo_2, Orbitron } from "next/font/google";
 import NavBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
+import "./globals.css";
 
 
 const geistSans = Geist({
